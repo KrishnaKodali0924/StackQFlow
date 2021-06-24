567509cf4517df8eee677795cf0d0402bfd80c6c
Krishna Kodali <kodali.krishna@publicissapient.com>
2021-06-24 12:25:08 +0530

    added sql and nosql database modules

---
+++ Mongo-db/src/main/java/com/SQFlow/nosql/client/App.java
@@ -0,0 +1,9 @@
+package com.SQFlow.nosql.client;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Team");
+	}
+
+}
---
+++ Postgres-db/src/main/java/com/SQFlow/sql/client/App.java
@@ -0,0 +1,9 @@
+package com.SQFlow.sql.client;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Team");
+	}
+
+}
