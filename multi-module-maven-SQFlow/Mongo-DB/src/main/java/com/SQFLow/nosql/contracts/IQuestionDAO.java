74f79eb48ae6a8698d19f46cd48366fc406e218f
Bitan Sarkar <bitan.sarkar@publicissapient.com>
2021-06-25 11:36:58 +0530

    QuestionDAO added

--- multi-module-maven-SQFlow/Mongo-DB/src/main/java/com/SQFLow/nosql/contracts/IQuestionDAO.java
+++ multi-module-maven-SQFlow/Mongo-DB/src/main/java/com/SQFLow/nosql/contracts/IQuestionDAO.java
@@ -1,5 +1,18 @@
 package com.SQFLow.nosql.contracts;
 
+import com.SQFLow.nosql.entity.Question;
+
 public interface IQuestionDAO {
+	public void insertOne(Question question);
+
+	public Question findQuestionByQId(int qid);
+
+	public Iterable<Question> findQuestionByUId(int uid);
+
+	public Iterable<Question> findAll();
+
+	public void addUpVote(int qid);
+
+	public void addDownVote(int qid);
 
 }
